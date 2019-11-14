4932858097bb8644328088425f706c69ec3b696f
Melyohin.AA <melyohin.aa@gmail.com>
2019-11-14 20:35:56 +0300

    Exceptions, Level, StaticGrid, StaticGridCell, BrickCell classes are added

---
+++ src/static_grid_cells.py
@@ -0,0 +1,32 @@
+﻿import pygame
+
+from src.base_classes import DrawableObject
+from src.exceptions import Exceptions
+
+# Это статические игровые объекты, применяемые в классе StaticGrid
+
+class StaticGridCell(DrawableObject): #abstract
+    cell_types_from_save_symbols = None
+
+    def init():
+        StaticGridCell.cell_types_from_save_symbols = { }
+        StaticGridCell.cell_types_from_save_symbols['B'] = BrickCell
+
+    def __init__(self, game, image, locx, locy):
+        super().__init__(game)
+        if not (isinstance(image, pygame.Surface) and isinstance(locx, int) and isinstance(locy, int)):
+            Exceptions.throw(Exceptions.argument_type)
+        self.image = image
+        self.locx = locx
+        self.locy = locy
+        self.rect = image.get_rect()
+        self.rect.x = locx << 6
+        self.rect.y = locy << 6
+
+    def process_draw(self):
+        self.game_object.screen.blit(self.image, self.rect)
+
+
+class BrickCell(StaticGridCell):
+    def __init__(self, game, image, locx, locy):
+        super().__init__(game, image, locx, locy)
