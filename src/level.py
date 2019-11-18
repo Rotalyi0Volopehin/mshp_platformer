c5fcd23270f065f0335448e65f63e730c2cc61db
Melyohin.AA <melyohin.aa@gmail.com>
2019-11-18 18:22:54 +0300

    Grid improvement

--- src/level.py
+++ src/level.py
@@ -2,6 +2,7 @@ import glob
 import pygame
 
 from src.static_grid import StaticGrid
+from src.io_tools import IO_Tools
 
 # Уровень и информация о нём
 # Каждый уровень в папке levels имеет свою папку, название которой должно быть "level_*" (* - параметр name)
@@ -9,10 +10,11 @@ from src.static_grid import StaticGrid
 class Level:
     def __init__(self, game, name):
         self.images = { }
-        lvl_path = "levels\\level_" + name + "\\"
+        slash = IO_Tools.sep_slash()
+        lvl_path = "levels{1}level_{0}{1}".format(name, slash)
         for img_path in glob.glob(lvl_path + "*.png"):
             image = pygame.image.load(img_path)
-            img_name = img_path[img_path.rfind('\\') + 1 : img_path.rfind('.')]
+            img_name = img_path[img_path.rfind(slash) + 1 : img_path.rfind('.')]
             self.images[img_name] = image
         lvl_file = open(lvl_path + "struct.txt")
         lvl_struct_lines = lvl_file.readlines()
