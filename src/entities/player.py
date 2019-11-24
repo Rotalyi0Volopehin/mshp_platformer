28f7889a30cc5a0a586f8f0608d0215cb9f82c45
Melyohin.AA <melyohin.aa@gmail.com>
2019-11-24 21:14:24 +0300

    Player conflict update

--- src/entities/player.py
+++ src/entities/player.py
@@ -5,21 +5,44 @@ from src.static_grid_cells.obstacle import Obstacle
 
 
 class Player(Entity):
+    speed = 1
+    jump_force = 10
+
     def __init__(self, game, image, posx, posy):
         super().__init__(game, image, posx, posy)
         self.move_left = self.move_top = self.move_right = self.move_bottom = False
+        self.left_collision = self.top_collision = self.right_collision = self.bottom_collision = False
+
+    def process_logic(self):
+        if self.move_left and not self.left_collision:
+            self.vx = -self.speed
+        if self.move_right and not self.right_collision:
+            self.vx = self.speed
+        if self.move_top and not self.top_collision:
+            self.vy = self.speed
+        if self.move_left and not self.left_collision:
+            self.move_left = False
+            self.vx = self.jump_force
+        self.move_left = self.move_top = self.move_right = self.move_bottom = False
+        self.left_collision = self.top_collision = self.right_collision = self.bottom_collision = False
+        if not self.bottom_collision:
+            self.apply_gravity_force(1)
 
     def on_collide(self, collisions):
         for collision in collisions:
             if isinstance(collision, Obstacle):
                 if collision.left and (self.vx < 0):
                     self.vx = 0
+                    self.left_collision = True
                 if collision.right and (self.vx > 0):
                     self.vx = 0
+                    self.right_collision = True
                 if collision.top and (self.vy < 0):
                     self.vy = 0
+                    self.top_collision = True
                 if collision.bottom and (self.vy > 0):
                     self.vy = 0
+                    self.bottom_collision = True
 
     def process_event(self, event):
         keydown = event.type == pygame.KEYDOWN
