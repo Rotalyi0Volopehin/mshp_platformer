a25893bbf52aa429a17824e6270eb8266dac9c40
Michail Kolpakov <kolpmikel@mail.ru>
2019-12-01 13:59:47 +0300

    Mapping flower, tube

---
+++ mapping.py
@@ -0,0 +1,12 @@
+def __init_statc_grid_cell_dict():
+    SaveSymbolRegister.static_grid_cell_dict = { }
+    SaveSymbolRegister.static_grid_cell_dict['F'] = Flower
+
+def __init_static_grid_cell_dict():
+    SaveSymbolRegister.static_grid_cell_dict = { }
+    SaveSymbolRegister.static_grid_cell_dict['T1'] = BottomTube
+
+def __init_static_grid_cell_dict():
+    SaveSymbolRegister.static_grid_cell_dict = { }
+    SaveSymbolRegister.static_grid_cell_dict['T2'] = UpperTube
+
