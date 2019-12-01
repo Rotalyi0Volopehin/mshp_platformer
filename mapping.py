3a40d4657e48292ef46936d23b5d7370f6c4c940
Michail Kolpakov <kolpmikel@mail.ru>
2019-12-01 14:03:00 +0300

    Mapping tube

--- mapping.py
+++ mapping.py
@@ -1,12 +1,7 @@
-def __init_statc_grid_cell_dict():
-    SaveSymbolRegister.static_grid_cell_dict = { }
-    SaveSymbolRegister.static_grid_cell_dict['F'] = Flower
+def __init_static_grid_cell_dict():
+SaveSymbolRegister.static_grid_cell_dict = { }
+SaveSymbolRegister.static_grid_cell_dict['T1'] = BottomTube
 
 def __init_static_grid_cell_dict():
-    SaveSymbolRegister.static_grid_cell_dict = { }
-    SaveSymbolRegister.static_grid_cell_dict['T1'] = BottomTube
-
-def __init_static_grid_cell_dict():
-    SaveSymbolRegister.static_grid_cell_dict = { }
-    SaveSymbolRegister.static_grid_cell_dict['T2'] = UpperTube
-
+SaveSymbolRegister.static_grid_cell_dict = { }
+SaveSymbolRegister.static_grid_cell_dict['T2'] = UpperTube
