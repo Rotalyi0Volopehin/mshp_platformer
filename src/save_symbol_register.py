6850ab932dfd047f6dd5432395b54eaa08408950
Петр Кузьмичев <peter.kuzmicheff@yandex.ru>
2019-12-08 10:41:01 +0300

    Fixed OS bug

--- src/save_symbol_register.py
+++ src/save_symbol_register.py
@@ -1,6 +1,6 @@
 ﻿# Импорт классов ячеек статической сетки
 from src.static_grid_cells.brick_cell import BrickCell
-from src.static_grid_cells.castle import Castle
+from src.static_grid_cells.castle import CastleEntry
 # Импорт классов сущностей
 from src.entities.fake_cloud import FakeCloud
 from src.entities.player import Player
@@ -18,7 +18,7 @@ class SaveSymbolRegister: #static
     def __init_static_grid_cell_dict():
         SaveSymbolRegister.static_grid_cell_dict = { }
         SaveSymbolRegister.static_grid_cell_dict['B'] = BrickCell #<- Пример регистрации класса ячейки статической сетки (блока)
-        SaveSymbolRegister.static_grid_cell_dict['E'] = Castle
+        SaveSymbolRegister.static_grid_cell_dict['E'] = CastleEntry
 
     def __init_entity_dict():
         SaveSymbolRegister.entity_dict = { }
--- src/static_grid_cells/castle.py
+++ src/static_grid_cells/castle.py
@@ -1,17 +1,21 @@
 import sys
 import pygame
+from src.io_tools import *
 from src.camera import Camera
 from src.constants import *
 from src.static_grid_cells.brick_cell import Obstacle
 from src.entities.player import Player
 
 
-class Castle(Obstacle):
+class CastleEntry(Obstacle):
     def __init__(self, game, image, locx, locy):
         super().__init__(game, image, locx, locy)
-        self.castle_image = pygame.image.load("%s/castle.png" % IMAGES_DIR)
+        slash = IO_Tools.sep_slash()
+        lvl_path = "levels{1}level_{0}{1}".format('0', slash)
+        sprites_dir = "{}sprites{}".format(lvl_path, slash)
+        self.castle_image = pygame.image.load(sprites_dir + "Castle.png")
         self.castle_rect = self.castle_image.get_rect()
-        self.castle_rect.bottomleft = self.rect.bottomleft
+        self.castle_rect.topleft = self.rect.topleft
 
     def collide_with(self, other_rigid_body):
         if isinstance(other_rigid_body, Player):
